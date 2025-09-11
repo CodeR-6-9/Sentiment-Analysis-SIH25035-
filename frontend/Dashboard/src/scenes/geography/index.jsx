0834230a85f7e939243cf941db755b4013bcb7ca
Hridesh Mehrotra <hridesh.mehrotra@gmail.com>
2025-09-11 14:28:50 +0530

    Fixed dashboard repo tracking

---
+++ frontend/Dashboard/src/scenes/geography/index.jsx
@@ -0,0 +1,16 @@
+import { Box, useTheme } from "@mui/material";
+import GeographyChart from "../../components/GeographyChart";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+
+    </Box>
+  );
+};
+
+export default Geography;
